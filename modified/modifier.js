88290fa08059d0493bff64ec71c37bda7f8c5052
Ben Perlmutter <90647379+mongodben@users.noreply.github.com>
2022-01-11 17:29:14 -0500

    add modified folder for additional tweaks

---
+++ modified/modifier.js
@@ -0,0 +1,20 @@
+const spec = require("./spec.json");
+const fs = require("fs");
+const { PROJECT_DIR } = require("../settings");
+const path = require("path");
+
+const trimWhiteSpace = (obj) => {
+  Object.keys(obj).forEach((key) => {
+    if (typeof obj[key] === "object") {
+      trimWhiteSpace(obj[key]);
+    } else if (typeof obj[key] === "string") {
+      obj[key] = obj[key].trim();
+    }
+  });
+  return obj;
+};
+
+const trimmedSpec = trimWhiteSpace(spec);
+
+const pathOut = path.join(PROJECT_DIR, "modified", "spec-trimmed.json");
+fs.writeFileSync(pathOut, JSON.stringify(trimmedSpec));
